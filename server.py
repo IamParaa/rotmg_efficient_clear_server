0e5db76accf815e172976c280f064eb8142fa8c8
IamParaa <66681274+IamParaa@users.noreply.github.com>
2021-12-13 19:31:07 +0100

    pw support added as string query parameter
    no clue if it works, client update soonTM

--- server.py
+++ server.py
@@ -1,52 +1,73 @@
+import re
 from flask import Flask, session, request
 import json
 import redis
 
 
 app = Flask(__name__)
-r = redis.Redis('localhost',charset="utf-8", decode_responses=True)
+_redis = redis.Redis('localhost',charset="utf-8", decode_responses=True)
 
 def fixedTemplate():
     data = {}
-    data['server'] = "..."
+    data['server'] = "undefined"
+    data['pw'] = "default"
     data['mapIdx'] = 0
     for i in range(128):
         data[i] = 0
     return data
 
-def groupId():
-    return "MUG"
-
 @app.route("/<lobby>", methods=["GET", "PUT", "POST"])
 def derp(lobby):
+
+    if lobby == "favicon.ico": #browser stuff...
+        return "lmao"
+
+    pw = request.args.get('pw')
+    if not pw:
+        print("pw did not get set, defaulting")
+        pw = "default"
+
     if request.method == "POST":
-        print("HERE")
+        data = {}
         try:
             data = request.get_json(force=True)
         except Exception as e:
-            print("FUCK", e)
+            print("Uknown Error with post Request, {}".format(e))
             data = {}
-        print("POSTED DATA", data)
+
+        roomExists = _redis.exists(lobby) == True
+        if roomExists == True:
+            if _redis.hget(lobby,'pw') != pw:
+                return "WRONG PASSWORD"
+
         d = fixedTemplate()
         for key in data.keys():
-            d[key] = data[key]
+            b_continue = False
+            if roomExists == True and key == 'pw':
+                b_continue = True
+            else:
+                d['pw'] = pw
+            if not b_continue:
+                d[key] = data[key]
         for key in d.keys():
-            r.hset(lobby, key, d[key])
+            _redis.hset(lobby, key, d[key])
         return "new lobby set"
 
     if request.method == "PUT":
         data = request.get_json(force=True)
         print("PUT DATA", data, type(data))
-        if r.exists(lobby):
+        if _redis.exists(lobby):
+            if pw != _redis.hget(lobby, 'pw'):
+                return "WRONG PASSWORD"
             clientMapIdx = data.pop('mapIdx')
             clientServer = data.pop('server')
-            serverMapIdx = r.hget(lobby, "mapIdx")
+            serverMapIdx = _redis.hget(lobby, "mapIdx")
             if clientMapIdx != serverMapIdx:
                 return "lobby existed, did not update though"
             for k in data.keys():
                 v = data[k]
-                r.hset(lobby, k, v)
-                r.expire(lobby, 1800)
+                _redis.hset(lobby, k, v)
+                _redis.expire(lobby, 1800)
             return "lobby existed"
         else:
             d = fixedTemplate()
@@ -54,24 +75,30 @@ def derp(lobby):
                 v = data[k]
                 d[k] = v
             for key in d.keys():
-                r.hset(lobby, key, d[key])
-            r.expire(lobby, 1800)
+                _redis.hset(lobby, key, d[key])
+            _redis.expire(lobby, 1800)
             return "lobby now exists and key set"
 
     if request.method == "GET":
-        if not r.exists(lobby):
+        if not _redis.exists(lobby):
             d = fixedTemplate()
+            d['pw'] = pw
             for key in d.keys():
-                r.hset(lobby, key, d[key])
+                _redis.hset(lobby, key, d[key])
+
+        if _redis.hget(lobby,"pw") != pw:
+            return "WRONG PASSWORD"
+
         data = {}
-        data['server'] = r.hget(lobby, "server")
-        data['mapIdx'] = r.hget(lobby, "mapIdx")
+        data['server'] = _redis.hget(lobby, "server")
+        data['mapIdx'] = _redis.hget(lobby, "mapIdx")
+
         circleIds = []
         for i in range(128):
-            circleIds.append(r.hget(lobby, i))
+            circleIds.append(_redis.hget(lobby, i))
         data['markerPiIdxs'] = circleIds
-        print("hmmm", data)
-        r.expire(lobby, 1800)
+       # print("hmmm", data)
+        _redis.expire(lobby, 1800)
         return json.dumps(data)
     
     return "UNSUPPORTED"
